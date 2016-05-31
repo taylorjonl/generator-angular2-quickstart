a686fa1b16ebf9e85a1812559dfcd8fff44a8abd
4tron <cuthbert.tim@hotmail.com>
2016-05-31 13:29:33 +0200

    added basic copying structure

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,40 +1,113 @@
-var generators = require('yeoman-generator');
+var generator = require('yeoman-generator');
 
-module.exports = generators.Base.extend();
-
-module.exports = gen.Base.extend({
+module.exports = generator.Base.extend({
 
     constructor: function () {
-        gen.Base.apply(this, arguments);
+        generator.Base.apply(this, arguments);
     },
 
     initializing: function () {
-        
+
     },
-    
+
     prompting: function () {
-        var done =  this.async();
+        var done = this.async();
+
+        this.prompt([
+            
+            // Application name
+            {
+                type: 'input',
+                name: 'appName',
+                message: 'What is the name of your application ? ',
+                default: this.config.get('appName') || 'New Application',
+            },
+            // Main application selector
+            {
+                type: 'input',
+                name: 'selectorName',
+                message: 'What is the name of your main component selector ? ',
+                default: this.config.get('selectorName') || 'my-app',
+            },
+            // Application Author
+            {
+                type: 'input',
+                name: 'appAuthor',
+                message: 'Author',
+                default: this.config.get('appAuthor')
+            },
+        ], function (answers) {
+
+            // Settings added to global yeoman config file: .yo-rc.json
+            this.config.set({
+                'appName': answers.appName,
+                'selectorName': answers.selectorName,
+                'appAuthor': answers.appAuthor
+            });
+
+            this.config.save();
+            done();
+        }.bind(this));
     },
-    
+
     configuring: function () {
 
     },
-    
+
     writing: {
-        
+
+        // Package.json
+        packagefile: () => {
+            this.fs.copyTpl(
+                this.templatePath('_package.json'),
+                this.destinationPate('package.json'),
+                {
+                    appName: this.config.get('appName'),
+                    appAuthor: this.config.get('appAuthor')
+                }
+            )
+        },
+
+        // Config files
+        configfiles: () => {
+            var filesArr = ['_systemjs.config', '_tsconfig.json', '_typings.json', '_styles.css']
+            filesArr.forEach(filesArr, (file) => {
+                this.copy(file, file.substring(1))
+            });
+        },
+
+        // Index file
+        indexfile: () => {
+            this.fs.copyTpl(
+                this.templatePath('_index.html'),
+                this.destinationPate('index.html'),
+                {
+                    selectorName: this.config.get('selectorName'),
+                    appName: this.config.get('appName'),
+                }
+            )
+        }
     },
-    
+
     conflicts: function () {
 
     },
-    
+
     install: function () {
         this.installDependencies({
-            skipInstall: this.options['skip-install'] // Skip on an update or test
+            skipInstall: this.options['skip-install'] 
         });
     },
-    
+
     end: function () {
-        
+
+        // Start server
+        if (this.options['npmstart']) {
+            this.spawnCommand('npm', ['start']);
+        }
+        // Open vscode
+        if (this.options['vscode']) {
+            this.spawnCommand('code', ['.']);
+        }
     }
 })
