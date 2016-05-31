2ebfb1e3c8a8d2809e8ff64deb85eb622b247831
4tron <cuthbert.tim@hotmail.com>
2016-05-31 15:23:13 +0200

    updated _app.component.ts

--- generators/app/templates/app/_app.component.ts
+++ generators/app/templates/app/_app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'my-app',
+  selector: '<%= selectorName %>',
   template: '<h1>My First Angular 2 App</h1>'
 })
 export class AppComponent { }
