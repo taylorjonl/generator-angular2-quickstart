9c0f81de9b009fefc571224205dded96d7e32a3a
4tron <cuthbert.tim@hotmail.com>
2016-05-31 11:54:18 +0200

    added index.js structure

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,3 +1,40 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend();
+
+module.exports = gen.Base.extend({
+
+    constructor: function () {
+        gen.Base.apply(this, arguments);
+    },
+
+    initializing: function () {
+        
+    },
+    
+    prompting: function () {
+        var done =  this.async();
+    },
+    
+    configuring: function () {
+
+    },
+    
+    writing: {
+        
+    },
+    
+    conflicts: function () {
+
+    },
+    
+    install: function () {
+        this.installDependencies({
+            skipInstall: this.options['skip-install'] // Skip on an update or test
+        });
+    },
+    
+    end: function () {
+        
+    }
+})
